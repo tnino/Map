5b926bb801bb9f65beb539c46eb72690d0d59773
tnino <30389051+tnino@users.noreply.github.com>
2018-05-23 21:26:09 +0200

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -1,13 +1,15 @@
+
+
 //Locations array
 var Locationaustin = [{
         restaurantname: 'Torchies Tacos',
-        lat: 330.245432, 
+        lat: 30.245432, 
         lng: -97.75152
     },
     {
-        restaurantname: 'PintHouse Pizza',
-        lat: 30.236083, 
-        lng: -97.795897
+        restaurantname: 'Gueros Austin',
+        lat: 30.249061, 
+        lng: -97.750048
     },
     {
         restaurantname: 'Zilker Park',
@@ -15,13 +17,64 @@ var Locationaustin = [{
         lng: -97.772859
     },
     {
-        restaurantname: 'Sway Thai Food',
-        lat: 30.250129, 
-        lng: -97.754559
+        restaurantname: 'Fresas Tacos al Carbon',
+        lat: 30.274474, 
+        lng: -97.752224
     },
     {
-        restaurantname: 'Uchi Sushi',
-        lat: 30.257514, 
-        lng: -97.759771
+        restaurantname: 'La Condesa',
+        lat: 30.265608, 
+        lng: -97.747803
     },
+    {
+        restaurantname: 'Taco Deli',
+        lat: 30.407702, 
+        lng: -97.713286
+    },
+    {
+        restaurantname: 'Maria Tacos',
+        lat: 30.245766, 
+        lng: -97.778283
+    },
+    {
+        restaurantname: 'Veracruz Tacos',
+        lat: 30.257933, 
+        lng: -97.725928
+    },
+    {
+        restaurantname: 'Cool Beans',
+        lat: 30.295504, 
+        lng: -97.741759
+    },
+    {
+        restaurantname: 'Paco Tacos ',
+        lat: 30.307426, 
+        lng: -97.705849
+    },
+    {
+        restaurantname: 'Amaya taco village',
+        lat: 30.319336, 
+        lng: -97.708140
+    },
+    {
+        restaurantname: 'Taco Shack',
+        lat: 30.462305, 
+        lng: -97.814788
+    },
+    {
+        restaurantname: 'El Chillito',
+        lat: 30.284150, 
+        lng: -97.718377
+    },
+    {
+        restaurantname: 'Truys Tacos',
+        lat: 30.197259, 
+        lng: -97.767025
+    },
+    {
+        restaurantname: 'Chuys TexMex',
+        lat: 30.263851, 
+        lng: -97.763268
+    },
+
 ];
