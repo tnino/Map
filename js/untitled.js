6dd8feb150f06afd02d8c10f640aa5e844519feb
tnino <30389051+tnino@users.noreply.github.com>
2018-05-24 00:13:36 +0200

    Update untitled.js

--- js/untitled.js
+++ js/untitled.js
@@ -1,18 +1,11 @@
+var Markerss = [];
 
-//Base code help provided by Udacity Javascript Design Patterns Course
-// Model
-// model for currentMarkers
-var currentMarkers = [];
-
-
-var go = function(tst) {
-    console.log(tst)
-}
 
 // ViewModel
-var ViewModel = function() {
+var ViewModel = function () {
 
     var self = this;
+
 
     this.filterLocation = ko.observableArray();
     self.filteredLocations = ko.observableArray();
@@ -21,11 +14,120 @@ var ViewModel = function() {
     self.currentLocation = ko.observable(this.locationList()[0]);
     self.searchField = ko.observable('');
     self.directionField = ko.observable('');
-    go(self.directionField )
+    self.maxdu = ko.observable('');
+    self.maxdu2 = ['10', '15', '20'];
+    this.currentduration = ko.observable('');
+    self.maxMode2 = ['DRIVING', 'WALKING', 'BICYCLING', 'TRANSIT'];
+    this.currentMode = ko.observable('');
+    console.log(this.currentMode())
+
+    //go(self.directionField )
     this.showLocation = ko.observable(true);
+    this.map;
+
+    this.duracion = ko.observable();
+    this.distancia = ko.observable();
+
+    self.go = function (tst) {
+        searchWithinTime()
+
+    }
+
+    //---- 
+    // This function allows the user to input a desired travel time, in order to get a tco quicker.
+    
+    function searchWithinTime() {
+        // Initialize the distance matrix service.
+        var distanceMatrixService = new google.maps.DistanceMatrixService;
+        var address = self.directionField();
+        // user needs to make sure that they enter a mailing adress or location.
+        if (address == '') {
+            window.alert('You must enter an address.');
+        } else {
+        
+            var origins = [];
+            console.log(Locationaustin)
+            for (var i = 0; i < Locationaustin.length; i++) {
+                origins[i] = {
+                    lat: Locationaustin[i].lat,
+                    lng: Locationaustin[i].lng
+                };
+            }
+            console.log(address)
+            var destination = address;
+            var mode = self.currentMode();
+            
+            distanceMatrixService.getDistanceMatrix({
+                origins: origins,
+                destinations: [destination],
+                travelMode: google.maps.TravelMode[mode],
+                unitSystem: google.maps.UnitSystem.IMPERIAL,
+            }, function (response, status) {
+                if (status !== google.maps.DistanceMatrixStatus.OK) {
+                    window.alert('Error was: ' + status);
+                } else {
+                    displayMarkersWithinTime(response);
+                }
+            });
+        }
+    }
+    // The distance will show only if the distance is less that the distance selected. 
+    function displayMarkersWithinTime(response) {
+        var maxDuration = self.currentduration();
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
+        
+        var atLeastOne = false;
+        for (var i = 0; i < origins.length; i++) {
+            var results = response.rows[i].elements;
+            for (var j = 0; j < results.length; j++) {
+                var element = results[j];
+                if (element.status === "OK") {
+                    atLeastOne = true;
+
+                   
+                    var distanceText = element.distance.text;
+                    console.log(distanceText)
+                    self.distancia = distanceText
+
+                    // Duration value is given in seconds so we make it MINUTES. We need both the value
+                    // and the text.
+                    var duration = element.duration.value / 60;
+                    console.log(duration)
+                    var durationText = element.duration.text;
+                    console.log(durationText)
+                    self.duracion = durationText
+                    console.log(self.duracion)
+                    document.getElementById('durationText').innerHTML = durationText;
+                    document.getElementById('distanceText').innerHTML = distanceText;
+                    if (duration <= maxDuration) {
+                        //the origin [i] should = the markers[i]
+                        //Locationaustin[i].setMap(map);
+
+                        var infowindow = new google.maps.InfoWindow({
+                            content: durationText + ' away, ' + distanceText +
+                                '<div><input type=\"button\" value=\"View Route\" onclick =' +
+                                '\"displayDirections("' + origins[i] + '");\"></input></div>'
+                        });
+        
+                        // when people click on the marker, wthe infowindow opens
+                        Locationaustin[i].infowindow = infowindow;
+                        google.maps.event.addListener(Locationaustin[i], 'click', function () {
+                            this.infowindow.close();
+                        });
+                    }
+                }
+            }
+        }
+        if (!atLeastOne) {
+            window.alert(' at this time, We could not find any locations within that distance!');
+        }
+    }
+    //----
+
 
     // filter Locationaustin based on keystroke
-    this.Locationaustin = ko.computed(function() {
+    this.Locationaustin = ko.computed(function () {
         var search = self.searchField().toLowerCase();
 
         for (var i = 0; i < Locationaustin.length; i++) {
@@ -41,14 +143,14 @@ var ViewModel = function() {
             } // if-else
         } // for
 
-        return ko.utils.arrayFilter(Locationaustin, function(location) {
+        return ko.utils.arrayFilter(Locationaustin, function (location) {
             return location.restaurantname.toLowerCase().indexOf(search) >= 0;
         });
 
     }, this);
 
     // Display the marker on the map based on user click on the location list
-    self.showLocation = function(location) {
+    self.showLocation = function (location) {
         for (var i = 0; i < Locationaustin.length; i++) {
             //var place = new Location(Locationaustin[i]);
             if (location.restaurantname == Locationaustin[i].restaurantname) {
@@ -62,7 +164,7 @@ var ViewModel = function() {
     }; // self.showLocation
 
 
-    this.setLocation = function(clickedLocation) {
+    this.setLocation = function (clickedLocation) {
         self.currentLocation(clickedLocation);
     };
 
@@ -70,16 +172,12 @@ var ViewModel = function() {
 };
 
 
-// Map function
-var map;
-
-//New array created for listed markers below.
 self.markers = [];
 
 var self = this;
 
-// Add function for foursquare API
-var populateInfoWindow = function(marker, infowindow) {
+// Wikipedia API information
+var populateInfoWindow = function (marker, infowindow) {
 
 
     if (infowindow.marker != marker) {
@@ -87,94 +185,38 @@ var populateInfoWindow = function(marker, infowindow) {
         infowindow.marker = marker;
         infowindow.open(map, marker);
 
-/*
-        //foursquare API
-        var CLIENT_SECRET = 'aIp3dfcSRlWjIsfXU3a-3DokC2Pgy6M8541JuZDuTKgaQPBECi3iiJjVgf3cO2AiGgniBKkTPKPYPyK4fFbqksKszj6bqYhc-3qbIN-zbnaGBUh_ciydGQTDyhryWnYx';
-        var CLIENT_ID = 'BsU7AeJoBHBgeCQ1E9R48g';
+        $.ajax({
+            url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + marker.restaurantname + "&prop=info&inprop=url&utf8=&format=json",
 
+            dataType: "jsonp",
+            success: function (response) {
+                console.log(response.query);
+                if (response.query.search.length >= 1) {
+                    infowindow.setContent('<div>' + '<h4>' + marker.restaurantname + '</h4> <br><p>' + response.query.search[0].snippet + '</p></div>');
+                } else {
 
-        var LL = '30.316056,-97.724944';  
-        //var FS_URL = URL + CLIENT_ID + CLIENT_SECRET;
-        var venue, address;
-
-var settings = {
-          'cache': false,
-          'dataType': "jsonp",
-          "async": true,
-          "crossDomain": true,
-          "url": "https://api.yelp.com/v3/businesses/search",
-          "method": "GET",
-          "headers": {
-              "accept": "application/json",
-              "Access-Control-Allow-Origin":"*"
-          }
-      }*/
-
-
-      /*{
-            url:'https://api.yelp.com/v3/businesses/search',
-            dataType: 'json',
-            data: {
-                limit: '1',
-                ll: LL,
-                client_id: CLIENT_ID,
-                client_secret: CLIENT_SECRET,
-                query: marker.restaurantname
+                    alert("no se encontro ninngun sitio llamado " + marker.restaurantname);
+                }
+            },
+            error: function () {
+                alert("Error retrieving search results, please refresh the page");
             }
-         }*/
 
-        //ajax call for data and info for foursquare
-        /*$.ajax(settings).done(function(response) {
-            console.log(response)
-              //displays info from each location
-              //create variables for content below
-              var address = response.response.venues[0].location.address;
-              var phonenumber = response.response.venues[0].contact.formattedPhone;
-              var twitter = response.response.venues[0].contact.twitter;
-              infowindow.setContent('<div>' + '<h4>' + marker.restaurantname + '</h4>' + address + '<br >' + phonenumber + '<br >' + 'Twitter:  ' + twitter + '</h4>' + '</div>');
-         })
-
-        //error for foursquare
-      .fail(function(e) {
-            alert("Oh No! Yelp API is currently unavailable. Please try again later.");
-        });*/
-
-  $.ajax({ 
-    url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+marker.restaurantname+"&prop=info&inprop=url&utf8=&format=json",
- 
-   dataType: "jsonp",
-   success: function(response) {
-       console.log(response.query);
-       if (response.query.search.length >= 1) {
-         infowindow.setContent('<div>' + '<h4>' + marker.restaurantname + '</h4> <br><p>'+response.query.search[0].snippet+'</p></div>');
-       }
-       else {
-
-    alert("no se encontro ninngun sitio llamado "+ marker.restaurantname);
-       }
-  },
-   error: function () {
-    alert("Error retrieving search results, please refresh the page");
-   }
- 
- });
-
+        });
 
 
     }
 
-    infowindow.addListener('closeclick', function() {
-    });
-
+    infowindow.addListener('closeclick', function () {});
 
 
 };
 
 
-    // Alerts for Map Error Messages
-    function googleError() {
-        alert("Google Maps API is currently unavailable. Please try again later.");
-    }
+//  if there is an error with the map, this message will show. 
+function googleError() {
+    alert("Google Maps API is currently not working right now, Please try again in a couple minutes.");
+}
 
 
 // Initiate the map
@@ -191,14 +233,15 @@ function initMap() {
             lng: -97.724944
         },
         zoom: 11,
-        styles,styles,
+        styles,
+        styles,
         mapTypeControl: false
 
     });
-  
 
-    self.setMarker = function(data) {
-        self.locationList().forEach(function(location) {
+
+    self.setMarker = function (data) {
+        self.locationList().forEach(function (location) {
             location.marker.setVisible(false);
         });
 
@@ -206,27 +249,27 @@ function initMap() {
 
         //makes markers bounce
         data.marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function() {
+        setTimeout(function () {
             data.marker.setAnimation(null);
         }, 2000);
         map.setCenter(data.marker.position);
     };
 
 
-    var toggleBounce = function(marker) {
+    var toggleBounce = function (marker) {
         if (marker.getAnimation() !== null) {
             marker.setAnimation(null);
         } else {
             marker.setAnimation(google.maps.Animation.BOUNCE);
-            setTimeout(function() {
+            setTimeout(function () {
                 marker.setAnimation(null);
             }, 1000);
         }
     };
 
 
-    function markerClickActions(marker) {
-        marker.addListener('click', function() {
+    function Mappins(marker) {
+        marker.addListener('click', function () {
             populateInfoWindow(this, largeInfoWindow);
         });
     }
@@ -237,13 +280,13 @@ function initMap() {
         var self = this;
         //self.setAnimation(google.maps.Animation.BOUNCE);
         this.marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function() {
+        setTimeout(function () {
             self.setAnimation(null);
         }, 1400);
         populateInfoWindow(this, largeInfoWindow);
     }
 
-    //This for loop use the location array to create an array of markers on the map.
+    // Loop to create an array of markers on the map.
     for (var i = 0; i < Locationaustin.length; i++) {
         var markerrestaurantname = Locationaustin[i].restaurantname;
         var markerLat = Locationaustin[i].lat;
@@ -256,26 +299,23 @@ function initMap() {
             },
             restaurantname: markerrestaurantname,
             id: 1,
-            animation: google.maps.Animation.DROP
+            animation: google.maps.Animation.BOUNCE
         });
         Locationaustin[i].marker = marker;
-        //marker.addListener calls to function below to animate and populate infowindow
-        marker.addListener('click', markerClickHandler);
-        markerClickActions(marker);
+        //animate map poimters
+        marker.addListener('click', Clickers);
+        Mappins(marker);
     }
 
-    function markerClickHandler() {
+    function Clickers() {
         var self = this;
         self.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function() {
+        setTimeout(function () {
             self.setAnimation(null);
         }, 1400);
         populateInfoWindow(this, largeInfoWindow);
     }
 
 
-
     // applyBindings for ViewModel
     ko.applyBindings(new ViewModel());
-
-}
